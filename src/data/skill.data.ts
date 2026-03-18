ed9be537141718b862c459eab50e3fbec8ff6cb9
Nguyen Huy Long <nguyenhuylong.dev@gmail.com>
2026-03-18 22:52:08 +0700

    init

---
+++ src/data/skill.data.ts
@@ -0,0 +1,28 @@
+import type { SkillItems } from "#/@types/skills.type";
+
+export const skillItems: SkillItems = [
+  {
+    name: "Listening",
+    img: "/listening-1024.png",
+    alt: "listening-image",
+    url: "/skills/listening",
+  },
+  {
+    name: "Speaking",
+    img: "/speaking_1024.png",
+    alt: "speaking-image",
+    url: "/skills/speaking",
+  },
+  {
+    name: "Reading",
+    img: "reading-1024.png",
+    alt: "reading-image",
+    url: "/skills/reading",
+  },
+  {
+    name: "Writing",
+    img: "writing_1024.png",
+    alt: "writing-image",
+    url: "/skills/writing",
+  },
+];
